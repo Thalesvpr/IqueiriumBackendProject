a363e8fe6fa2f37ab12fd6a51918f9e6b9aab35c
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-27 00:55:47 -0300

    feat(controller): adicionar ProductController com endpoints básicos

---
+++ Src/Api/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace IqueiriumBackendProject.Src.Api.Controllers
+{
+    [Route("product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/product
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/product/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/product/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
