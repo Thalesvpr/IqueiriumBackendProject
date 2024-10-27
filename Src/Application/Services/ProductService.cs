f8f6e0bb02dfcef3e5dfa060d620d682076fe1a6
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-27 01:54:16 -0300

    feat(service): adicionar ProductService com método para criação de produtos
    Implementa o ProductService com o método AddProductAsync, que converte CreateProductDto em ProductEntity, adiciona ao contexto e salva as alterações no banco de dados.

---
+++ Src/Application/Services/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using IqueiriumBackendProject.Src.Domain.Entities;
+using IqueiriumBackendProject.Src.Application.Dtos;
+using IqueiriumBackendProject.Src.Infrastructure.Data;
+using System.Threading.Tasks;
+
+namespace IqueiriumBackendProject.Src.Application.Services
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ProductEntity> AddProductAsync(CreateProductDto productDto)
+        {
+            var product = new ProductEntity
+            {
+                Title = productDto.Title,
+                Description = productDto.Description,
+                Price = productDto.Price,
+                Status = productDto.Status,
+                CreatedAt = productDto.CreatedAt
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+}
