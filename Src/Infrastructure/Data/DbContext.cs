7f3ad25b7fce93e2551975f40657a9efb8a1785b
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-27 00:55:23 -0300

    feat(database): adicionar DbContext básico para conexão com o banco de dados

---
+++ Src/Infrastructure/Data/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Iquirium_projeto_backend_dotnet.Src.Infrastructure.Data.DbContext
+{
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<YourEntity> YourEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
