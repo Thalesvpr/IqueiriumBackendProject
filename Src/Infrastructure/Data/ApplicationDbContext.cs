46e19a410cd9f5f3b602e6403de1f7b0c98c0a6f
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2024-10-27 01:51:44 -0300

    refactor(database): renomear DbContext para ApplicationDbContext

---
+++ Src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IqueiriumBackendProject.Src.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IqueiriumBackendProject.Src.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configurações adicionais, se necessário
+        }
+    }
+}
