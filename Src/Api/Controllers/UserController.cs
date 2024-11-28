1864b151f7767a25d6e201c8f9d70b6da7ae3501
mcocenzo <mcocenzo@gmail.com>
2024-11-27 23:40:44 -0300

    docs: Adicionada documentação dos métodos CRUD no controlador UserController

--- Src/Api/Controllers/UserController.cs
+++ Src/Api/Controllers/UserController.cs
@@ -10,11 +10,26 @@ namespace IqueiriumBackendProject.Src.Api.Controllers
     {
         private readonly UserService _userService;
 
+        /// <summary>
+        /// Inicializa uma nova instância do controlador <see cref="UserController"/>.
+        /// </summary>
+        /// <param name="userService">
+        /// Instância do <see cref="UserService"/> usada para acessar e manipular os dados do usuário.
+        /// </param>
         public UserController(UserService userService)
         {
             _userService = userService;
         }
 
+        /// <summary>
+        /// Endpoint para registrar um novo usuário no sistema.
+        /// </summary>
+        /// <param name="userDto">
+        /// Objeto contendo os dados necessários para registrar um novo usuário, como nome, email, senha, etc.
+        /// </param>
+        /// <returns>
+        /// Uma resposta HTTP 201 (Created) com o usuário criado e um link para o próprio recurso recém-criado.
+        /// </returns>
         [HttpPost("register")]
         public async Task<ActionResult<UserResponseDTO>> Register(UserRegisterDTO userDto)
         {
@@ -22,6 +37,16 @@ namespace IqueiriumBackendProject.Src.Api.Controllers
             return CreatedAtAction(nameof(Register), new { id = createdUser.Id }, createdUser);
         }
 
+        /// <summary>
+        /// Endpoint para autenticar um usuário com base no email e senha fornecidos.
+        /// </summary>
+        /// <param name="loginDto">
+        /// Objeto contendo as credenciais de login do usuário, como email e senha.
+        /// </param>
+        /// <returns>
+        /// Uma resposta HTTP 200 (OK) com os dados do usuário autenticado se as credenciais forem válidas,
+        /// ou uma resposta HTTP 401 (Unauthorized) com uma mensagem de erro se as credenciais forem inválidas.
+        /// </returns>
         [HttpPost("login")]
         public async Task<ActionResult<UserResponseDTO>> Authenticate(UserLoginDTO loginDto)
         {
