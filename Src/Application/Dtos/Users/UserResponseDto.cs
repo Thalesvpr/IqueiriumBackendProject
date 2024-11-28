1e7d048b06eb58dd4d0cf13947ecd19789b62e50
mcocenzo <mcocenzo@gmail.com>
2024-11-28 16:29:23 -0300

    docs: Adicionada documentação para o DTO UserResponseDTO

--- Src/Application/Dtos/Users/UserResponseDto.cs
+++ Src/Application/Dtos/Users/UserResponseDto.cs
@@ -1,5 +1,8 @@
 ﻿namespace IqueiriumBackendProject.Src.Application.Dtos.Users
 {
+    /// <summary>
+    /// DTO usado para retornar as informações de um usuário.
+    /// </summary>
     public class UserResponseDTO
     {
         public int Id { get; set; }
